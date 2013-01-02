c8f7dd731c7538c44a44c8647465125f25b4d746
Vinh Nguyen <kureikain@gmail.com>
2013-01-02 00:21:15 -0800

    Parse meta data for funny mama

--- meme/parser/funnymama.rb
+++ meme/parser/funnymama.rb
@@ -3,23 +3,43 @@ module Meme
   module Parser
     
     class Funnymama < Meme::Parser::Base
-    	def read_page page
-          meme = []
-          meme_on_page = page.search("article.post > .post-content > .post-img > a");
-          meme_on_page.each do |d|
-            meme.push(
-              {
-                :url => d['href'], 
-                :src => build_photo_url(d.children[1]['src']),
-                :id  => get_meme_id(d['href']),                
-                # :info => {
-                #   :comment =>
-                #   :like => 
-                # }
-              }
-            ) 
+    	
+      def read_page page
+        meme = []
+        meme_on_page = page.search("article.post > .post-content")# > .post-img > a");
+        meme_on_page.each do |d|
+          ameme = Hash.new
+
+          d.css(".post-img a").each do |node|
+            pp node
+            ameme[:url] = node['href']
+            ameme[:src] = build_photo_url(node.children[1]['src'])
+            ameme[:id]  = get_meme_id(node['href'])
+            ameme[:info] = Hash.new 
+            ameme[:info][:share] = Hash.new
           end
-          meme
+              
+          info_type = 0;
+          d.css(".post-info .post-info-shares li > span").each do |node|
+            pp node.children 
+
+            case info_type
+              when 0
+                ameme[:info][:comment] = node.children.to_s
+              when 1                  
+                ameme[:info][:like] = node.children.to_s
+              when 2                  
+                ameme[:info][:share][:facebook] = node.children.to_s
+              when 3                  
+                ameme[:info][:share][:twitter] = node.children.to_s
+            end
+
+            info_type = info_type + 1    
+
+          end
+          meme.push(ameme) 
+        end
+        meme
       end
 
       def fetch(section, start_id=0, end_id=0, quantity=10)
