e39de8fdc9d913272f6d889ee9dd85aaf7032557
Vinh Nguyen <kureikain@gmail.com>
2013-01-02 13:40:10 -0800

    parse 9gag data with new method use CSS selector instead of DOm traversing

--- meme/parser/ninegag.rb
+++ meme/parser/ninegag.rb
@@ -7,20 +7,37 @@ module Meme
           meme = []
           meme_on_page = page.search("#entry-list > #entry-list-ul > .entry-item");
           meme_on_page.each do |d|
-            meme.push(
-              {
-                :url => d['data-url'], 
-                :src => build_photo_url(d['gagid'], 700, :b), 
-                :id  => d['gagid'].to_i,
-                # 9gag follows a pattern for its pictur. d.children[1]['src'],
-                #http://d24w6bsrhbeh9d.cloudfront.net/photo/6087941_460s.jpg
-                # the pattern may change in future so it's better to build this dynamically
-                # :info => {
-                #   :comment =>
-                #   :like => 
-                # }
-              }
-            ) 
+            ameme = Hash.new
+
+            ameme[:url] = d['data-url']
+            ameme[:src] = build_photo_url(d['gagid'], 700, :b)
+            ameme[:id]  = d['gagid'].to_i
+            ameme[:comment_url] = comment_url(d['gagid'].to_i)
+            ameme[:info] = Hash.new 
+            ameme[:info][:share] = Hash.new
+
+            info_type = 0
+            d.css(".info .actions-wrap p span").each do |node|
+              pp node.children
+
+              case info_type
+                when 0
+                  ameme[:info][:comment] = node.children.to_s
+                when 1                  
+                  ameme[:info][:like] = node.children.to_s                
+              end
+              info_type = info_type + 1
+            end
+
+            d.css(".info .actions-wrap .sharing-box .b2-widget-count > div.b2-widget-val").each do |node|
+              ameme[:info][:share][:twitter] = node.children.to_s                    
+            end
+
+            d.css(".info .actions-wrap .sharing-box .facebook_share_count_inner").each do |node|
+              ameme[:info][:share][:facebook] = node.children.to_s                    
+            end
+
+            meme.push(ameme) 
           end
           meme
       end
@@ -45,15 +62,29 @@ module Meme
           end
           response = JSON.parse(page)
           meme =Array.new
-          response['ids'].each do |id|
+          # response['ids'].each do |id|
+          #   meme.push({
+          #     :url => build_post_url(id),
+          #     :src => build_photo_url(id, 700, :b),
+          #     :id => id.to_i
+          #   })
+
+          # end
+
+          response['items'].each do |k, v|
             meme.push({
               :url => build_post_url(id),
               :src => build_photo_url(id, 700, :b),
               :id => id.to_i
             })
+            
           end        
           meme
         end        
+      end
+
+      def comment_url meme_id
+        "https://www.facebook.com/plugins/comments.php?api_key=111569915535689&locale=en_US&sdk=joey&channel_url=https%3A%2F%2Fs-static.ak.facebook.com%2Fconnect%2Fxd_arbiter.php%3Fversion%3D18%23cb%3Df4ae20df94368a%26origin%3Dhttps%253A%252F%252F9gag.com%252Ff347388e2f6e8%26domain%3D9gag.com%26relation%3Dparent.parent&numposts=10&width=320&href=http%3A%2F%2F9gag.com%2Fgag%2F#{meme_id}"
       end
 
       private
